e737a324ea8c25c875300a8cc94591df7e72c3cc
VEETS5 <torinavito5@gmail.com>
2024-04-10 12:59:59 -0400

    Added new logo svg new favicon new logos in public for reference

--- killdozerapp/src/App.js
+++ killdozerapp/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bro if you dont fix this shit its over!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://killdozer.club/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Killdozer
         </a>
       </header>
     </div>
